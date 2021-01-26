fe78b3151582c3e3a167a0c399dccab814f04dfd
Alexander Jungwirth <jungwirth.research@gmail.com>
2021-01-26 09:54:13 +0100

    Added python files

---
+++ model.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, hidden_sizes=[512, 256, 256], drop_p=0.05):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            hidden_sizes (int)[]: list of integers, the sizes of the hidden layers
+            drop_p (float): dropout probability
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+
+        self.hidden_layers = nn.ModuleList([nn.Linear(state_size, hidden_sizes[0])])
+        
+        # Add a variable number of more hidden layers
+        layer_sizes = zip(hidden_sizes[:-1], hidden_sizes[1:])
+        self.hidden_layers.extend([nn.Linear(h1, h2) for h1, h2 in layer_sizes])
+        
+        self.output = nn.Linear(hidden_sizes[-1], action_size)
+        
+        self.dropout = nn.Dropout(p=drop_p)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        for each in self.hidden_layers:
+            state = F.relu(each(state))
+            state = self.dropout(state)
+        return self.output(state)
